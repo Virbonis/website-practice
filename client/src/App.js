3b7121a33b179460f8d3f6d4fc000e9f16f82f17
virbonis <hertantodicky@outlook.com>
2023-12-10 12:16:51 +0700

    Starter Project

---
+++ client/src/App.js
@@ -0,0 +1,9 @@
+import React from "react"
+import "./App.css"
+import CSSPractice from "./pages/css-practice"
+
+function App() {
+  return <CSSPractice />
+}
+
+export default App
---
+++ client/src/pages/css-practice/index.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+const CSSPractice = () => {
+  return (
+    <>
+      <div>A</div>
+    </>
+  )
+}
+
+export default CSSPractice
